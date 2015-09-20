74b5a1ff02f6a5b4050d1c87b28f70bf6d3401ef
Twero <twero001@gmail.com>
2015-09-20 13:32:12 +0800

    Creating basic Python codes
    I've created this codes with very basic syntax.

---
+++ djangogirls/MeFirstPython.py
@@ -0,0 +1,41 @@
+#print("wadafa")
+list=["yeee","yeee","yee","ye"]
+#print(list)
+#print(list[0])
+
+def hi():
+	print("empty")
+	
+def hi(wa):
+	
+	inp=input("Enter Some Int Dude:")
+	
+
+#	inp2=input("Enter Some Int Dude2:")
+	
+	if int(inp) ==1 :
+		print("true"+wa)
+	else:
+		print("false"+wa)
+
+
+hi("asd")
+
+
+list2=["a","b","c"]
+
+
+
+for name in list2:
+	print(name)
+
+
+
+inp3=input("Enter Numbers of friends you want:")
+
+friends=["wada"]
+for c in range(1,int(inp3)):
+	friends.append(input());	
+	print(friends[c])
+
+
